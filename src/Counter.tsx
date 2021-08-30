15f8a199e935ac1c0a7e9ddac36fe8b6bfb97c2f
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-08-30 15:41:36 +0900

    learn useRef

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -35,12 +35,21 @@ const Counter = () => {
     renderTimes.current++;
   });
 
+  const ref = useRef<HTMLInputElement>(null!);
+  const focusInput = () => {
+    // if (ref.current != null) ref.current.focus();
+    // ref.current?.focus();
+    ref.current.focus();
+  }
+
   return (
     <div>
       <p>value: {value}</p>
       <button onClick={increment}>+1</button>
       <button onClick={decrement}>-1</button>
       <p>This component was re-rendered {renderTimes.current} times.</p>
+      <input ref={ref} type="text" />
+      <button onClick={focusInput}>Click.</button>
     </div>
   );
 }
